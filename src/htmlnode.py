d8775d6cfac5c1104ac5a8d2b5a25812cb2079f6
phynnsoup <phynnigan@gmail.com>
2025-05-25 21:19:27 +1000

    parent node plus updated tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,11 +5,39 @@ class HTMLNode:
         self.children = children
         self.props = props
     def to_html(self):
-        raise NotImplemented
+        raise NotImplementedError
     def props_to_html(self):
-        props_string = "" 
+        props_string = ""
+        if self.props == None:
+            return props_string
         for prop, value in self.props.items():
             props_string += f" {prop}=\"{value}\""
         return props_string
+    
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None or self.value == "":
+                raise ValueError("no value given")
+        elif self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("no tag given")
+        if self.children == None or type(self.children) != list:
+            raise ValueError("no or incorrect type given for child")
+        content = list(map(lambda node: node.to_html(), self.children))
+        return f"<{self.tag}{self.props_to_html()}>{"".join(content)}</{self.tag}>"
