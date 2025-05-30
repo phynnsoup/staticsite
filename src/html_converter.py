dfbf2acc2d4db469522e6791fe1747d8c79848b1
phynnsoup <phynnigan@gmail.com>
2025-05-30 19:07:11 +1000

    getting sick of this exercise and it's unpythonic solutions

--- src/html_converter.py
+++ src/html_converter.py
@@ -3,17 +3,17 @@ from htmlnode import *
 
 
 def text_node_to_html_node(text_node):
-    match (text_node):
+    match (text_node.text_type):
          case TextType.TEXT:
-              return LeafNode(None, text_node.value)
+              return LeafNode(None, text_node.text)
          case TextType.BOLD:
-              return LeafNode("b", text_node.value)
+              return LeafNode("b", text_node.text)
          case TextType.ITALIC:
-              return LeafNode("i", text_node.value)
+              return LeafNode("i", text_node.text)
          case TextType.CODE:
-              return LeafNode("code", text_node.value)
+              return LeafNode("code", text_node.text)
          case TextType.LINK:
-              return LeafNode("a", text_node.value, {"href": text_node.url})
+              return LeafNode("a", text_node.text, {"href": text_node.url})
          case TextType.IMAGE:
               return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
          case _:
--- src/textnode.py
+++ src/textnode.py
@@ -19,3 +19,6 @@ class TextNode:
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+
+            
